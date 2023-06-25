d3caa2bd7289c07672a73053441fa6dcbcaaf1db
hannah <hannah.l.curran@gmail.com>
2023-06-25 15:31:11 +0100

    layout and initial stages

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 /*----- constants -----*/
 
+let cards = [
+    {value: '2', image: './images/diamonds/diamonds-A.svg' },
+    {value: '2', image: './images/diamonds/diamonds-A.svg' },
+    {value: '3', image: './images/diamonds/diamonds-r03.svg'},
+    {value: '3', image: './images/diamonds/diamonds-r03.svg'},
+    {value: '4', image: './images/diamonds/diamonds-r04.svg'},
+    {value: '4', image: './images/diamonds/diamonds-r04.svg'},
+    {value: '5', image: './images/diamonds/diamonds-r05.svg'},
+    {value: '5', image: './images/diamonds/diamonds-r05.svg'},
+    {value: '6', image: './images/diamonds/diamonds-r06.svg'},
+    {value: '6', image: './images/diamonds/diamonds-r06.svg'},
+    {value: 'Ace', image: './images/diamonds/diamonds-A.svg' },
+    {value: 'Ace', image: './images/diamonds/diamonds-A.svg' },
+]
 
 /*----- app's state (variables) - the variables that need to be global to the application and accessible to other functions we write-----*/
 //use arrays and objects
