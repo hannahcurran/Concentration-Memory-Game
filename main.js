218e76df64737528ed42989953a9e15c5d60c150
hannah <hannah.l.curran@gmail.com>
2023-06-25 20:09:09 +0100

    initial layout and work on shuffling array

--- main.js
+++ main.js
@@ -1,63 +1,60 @@
 /*----- constants -----*/
 
-// let cards = [
-//     {value: '2', image: './images/diamonds/diamonds-A.svg' },
-//     {value: '2', image: './images/diamonds/diamonds-A.svg' },
-//     {value: '3', image: './images/diamonds/diamonds-r03.svg'},
-//     {value: '3', image: './images/diamonds/diamonds-r03.svg'},
-//     {value: '4', image: './images/diamonds/diamonds-r04.svg'},
-//     {value: '4', image: './images/diamonds/diamonds-r04.svg'},
-//     {value: '5', image: './images/diamonds/diamonds-r05.svg'},
-//     {value: '5', image: './images/diamonds/diamonds-r05.svg'},
-//     {value: '6', image: './images/diamonds/diamonds-r06.svg'},
-//     {value: '6', image: './images/diamonds/diamonds-r06.svg'},
-//     {value: 'Ace', image: './images/diamonds/diamonds-A.svg' },
-//     {value: 'Ace', image: './images/diamonds/diamonds-A.svg' },]
+const cards = [
+        {value: '2', image: 'css/card-library/images/diamonds/diamonds-A.svg'},
+        {value: '2', image: 'css/card-library/images/diamonds/diamonds-A.svg'},
+        {value: '3', image: 'css/card-library/images/diamonds/diamonds-r03.svg'},
+        {value: '3', image: 'css/card-library/images/diamonds/diamonds-r03.svg'},
+        {value: '4', image: 'css/card-library/images/diamonds/diamonds-r04.svg'},
+        {value: '4', image: 'css/card-library/images/diamonds/diamonds-r04.svg'},
+        {value: '5', image: 'css/card-library/images/diamonds/diamonds-r05.svg'},
+        {value: '5', image: 'css/card-library/images/diamonds/diamonds-r05.svg'},
+        {value: '6', image: 'css/card-library/images/diamonds/diamonds-r06.svg'},
+        {value: '6', image: 'css/card-library/images/diamonds/diamonds-r06.svg'},
+        {value: 'Ace', image: 'css/card-library/images/diamonds/diamonds-A.svg'},
+        {value: 'Ace', image: 'css/card-library/images/diamonds/diamonds-A.svg'},]
 
-let cards = [
-        {value: '2', image: './images/diamonds/diamonds-A.svg' },
-        {value: '2', image: './images/diamonds/diamonds-A.svg' },
-        {value: '3', image: './images/diamonds/diamonds-r03.svg'},
-        {value: '3', image: './images/diamonds/diamonds-r03.svg'},
-        {value: '4', image: './images/diamonds/diamonds-r04.svg'},
-        {value: '4', image: './images/diamonds/diamonds-r04.svg'},
-        {value: '5', image: './images/diamonds/diamonds-r05.svg'},
-        {value: '5', image: './images/diamonds/diamonds-r05.svg'},
-        {value: '6', image: './images/diamonds/diamonds-r06.svg'},
-        {value: '6', image: './images/diamonds/diamonds-r06.svg'},
-        {value: 'Ace', image: './images/diamonds/diamonds-A.svg' },
-        {value: 'Ace', image: './images/diamonds/diamonds-A.svg' },]
+ let cardEls = document.querySelectorAll('.card');
+// let firstGuess = null;
 
-let cardEls = document.querySelectorAll('.card');
-let firstGuess = null;
+// cardEls.forEach(function (el, index) {
+//         el.addEventListener('click', function() {
+//                 let clickedCard = cards[index]
+//                 el.setAttribvute('src', clickedCard.image)
 
-shuffle(cards);
+//                 if (firstGuess === null) {
+//                         firstGuess = index;
+//                         }else {
+//                                 if(cards[firstGuess].value === cards[index].value){
+//                                         alert('Matched Pair!')
+//                                 }else {
+//                                         alert('Not a Match')
 
-cardEls.forEach(function (el, index) {
-        el.addEventListener('click', function( {
-                let clickedCard = cards[index]
-                el.setAttribvute('src', clickedCard.image)
+        //                         }
+        //                 }
+        // })) 
 
-                if (firstGuess === null) {
-                        firstGuess = index;
-                        }else {
-                                if(cards[firstGuess].value === cards[index].value){
-                                        alert('Matched Pair!')
-                                }else {
-                                        alert('Not a Match')
-
-                                }
-                        }
-        }))
-})
 /*----- app's state (variables) - the variables that need to be global to the application and accessible to other functions we write-----*/
 //use arrays and objects
 
 
 /*----- cached element references -----*/
-
+const playAgainBtn = document.querySelector('button');
 
 /*----- event listeners -----*/
-
+playAgainBtn.addEventListener('click', init);
 
 /*----- functions -----*/
+function shuffle(array) {
+        let currentIndex= array.length, randomIndex;
+        while (currentIndex !=0){
+                randomIndex = Math.floor(Math.random()* currentIndex);
+                currentIndex--;
+             [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        
+        }
+        return array;
+}
+
+shuffle(cards);
