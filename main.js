d5356fe2f5da0250bcb29c06de67ed585e5d52b5
hannah <hannah.l.curran@gmail.com>
2023-06-25 14:58:11 +0100

    general layout and initial stages

---
+++ main.js
@@ -0,0 +1,14 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) - the variables that need to be global to the application and accessible to other functions we write-----*/
+//use arrays and objects
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
