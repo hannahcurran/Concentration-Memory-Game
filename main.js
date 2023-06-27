586bf554c3273bc0aa1078775bdd85d881bbc42d
hannah <hannah.l.curran@gmail.com>
2023-06-27 12:00:58 +0100

    begin working on countdown

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*----- constants -----*/
 
 const cards = [
-        {value: '2', image: 'css/card-library/images/diamonds/diamonds-A.svg'},
+        {value: '2', image: 'css/card-library/images/diamonds/diamonds-r02.svg'},
         {value: '3', image: 'css/card-library/images/diamonds/diamonds-r03.svg'},
         {value: '4', image: 'css/card-library/images/diamonds/diamonds-r04.svg'},
-        {value: '2', image: 'css/card-library/images/diamonds/diamonds-A.svg'},
+        {value: '2', image: 'css/card-library/images/diamonds/diamonds-r02.svg'},
         {value: '5', image: 'css/card-library/images/diamonds/diamonds-r05.svg'},
         {value: 'Ace', image: 'css/card-library/images/diamonds/diamonds-A.svg'},
         {value: '5', image: 'css/card-library/images/diamonds/diamonds-r05.svg'},
@@ -15,6 +15,7 @@ const cards = [
         {value: 'Ace', image: 'css/card-library/images/diamonds/diamonds-A.svg'},
        ]
 
+
  const cardEls = document.querySelectorAll('.card');
 
  //getting cards to flip over and identify if they match
@@ -24,7 +25,7 @@ let firstGuess = null;
 cardEls.forEach(function (el, guess) {
         el.addEventListener('click', function() {
          if(guess === firstGuess){
-                return;
+        return;
          };       
         let clickedCard = cards[guess];
                 
@@ -33,7 +34,7 @@ cardEls.forEach(function (el, guess) {
             firstGuess = guess;
             }else {
             if(cards[firstGuess].value === cards[guess].value){
-            alert('Matched Pair!')
+                alert('Matched Pair!') // need matched cards to stay turned over and next turn to begin.
             }else {
             setTimeout(function(){
                 cardEls[firstGuess].setAttribute('src', 'css/card-library/images/red.svg');
@@ -51,6 +52,7 @@ cardEls.forEach(function (el, guess) {
 
 /*----- cached element references -----*/
 const playAgainBtn = document.querySelector('button');
+const countdownEl = document.getElementById('countdown'); 
 
 /*----- event listeners -----*/
 playAgainBtn.addEventListener('click', init);
@@ -58,12 +60,30 @@ playAgainBtn.addEventListener('click', init);
 /*----- functions -----*/
 
 //initialize all state then call render()
-function init(){
+// function init(){
 
+// }
+
+function render(){
+        renderCountdown(function(){
+        renderResults();
+        })
 }
 
-function renderCard(cb){
-
+ function renderCountdown(cb){
+      let count = 30;
+       countdownEl.style.visibility = 'visible';
+       countdownEl.innerText = count;
+       const timerId = setInterval(function(){
+                 count --;
+                 if (count) {
+                         countdownEl.innerText = count; 
+                         }else{
+                        clearInterval(timerId);
+                         countdownEl.style.visbility= 'hidden';
+                         cb();
+                      }
+         }, 1000);
 }
 
 // function shuffle(array) {
